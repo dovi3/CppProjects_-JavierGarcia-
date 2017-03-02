7c66e6e9089e592c3b2805fbe89ed92d18e189c6
Javier Garcia <javiergarciagomez@S3-06>
2017-03-02 13:57:11 +0100

    Intentando hacer el ejercicio

---
+++ DynArray/DynArray.cc
@@ -0,0 +1,35 @@
+#include "DynArray.hh"
+
+
+DynArray::DynArray() 
+{
+	m_data = new int[DYN_ARRAY_DEFAULT_SIZE];
+	m_capacity = DYN_ARRAY_DEFAULT_SIZE;
+	m_size = 0;
+};
+
+DynArray::DynArray(size_t size) 
+{
+	m_data = new int[size];
+};
+
+DynArray::DynArray(size_t size, const int &value)
+{
+
+
+	
+};
+
+DynArray::DynArray(int *arr, size_t size)
+{
+	
+	arr = new int[size];
+	m_capacity = size;
+	m_size = 0;
+};
+
+
+DynArray::~DynArray(void) 
+{
+	delete m_data;
+};
